8f762cd467de1a5cd6c926d9d49db091bde0f295
José Ribeiro <hello@jlbribeiro.com>
2016-04-01 05:47:09 +0100

    Adding a reconnect token to Player
    This guarantees there's no possibility of name collision while still
    allowing Players to reconnect in the case of connection loss.

--- src/server/src/player.js
+++ src/server/src/player.js
@@ -1,7 +1,26 @@
+import Crypto from 'crypto';
+
+const TOKEN_SIZE = 8;
+
 class Player {
-  constructor ({name = '', pic = ''}) {
+  constructor ({name = '', pic = '', token = false}) {
     this._name = name;
     this._pic = pic;
+    this._token = token;
+
+    if (!this._token) {
+      /* Doing this synchronously since this is a constructor; it will block
+       * until there's enough entropy, but under normal circumstances that
+       * shouldn't take more than a few millis.
+       *
+       * https://nodejs.org/dist/latest-v5.x/docs/api/crypto.html#crypto_crypto_randombytes_size_callback
+       */
+      this._token = Crypto.randomBytes(TOKEN_SIZE).toString('hex');
+    }
+  }
+
+  checkToken(challengingToken) {
+    return this._token === challengingToken;
   }
 
   get name() {
@@ -10,6 +29,10 @@ class Player {
 
   get pic() {
     return this._pic;
+  }
+
+  get token() {
+    return this._token;
   }
 
   toJSON() {
